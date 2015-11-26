8a8317ab910dfdf0dad2a50fc797580fd25b21fa
Mariano Guerra <mariano@marianoguerra.org>
2015-11-26 19:53:02 +0100

    initial implementation of frontend counter

--- src/om_next_e2e/ui.cljs
+++ src/om_next_e2e/ui.cljs
@@ -3,15 +3,45 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(defonce app-state (atom {:counter 0}))
+
+(defmulti read om/dispatch)
+(defmulti mutate om/dispatch)
+
+(defn get-state-key [{:keys [state]} key]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      :not-found)))
+
+(defmethod read :default [env key params]
+  (get-state-key env key))
+
+(defmethod mutate 'ui/increment [{:keys [state]} _ {:keys [value]}]
+  {:value {:keys [:counter]}
+   :action (fn [] (swap! state
+                         #(update % :counter (fn [old] (+ old value)))))})
+
+(def reconciler
+  (om/reconciler
+    {:state app-state :parser (om/parser {:read read :mutate mutate})}))
+
+(defn mutate! [query]
+  (om/transact! reconciler query))
+
 (enable-console-print!)
 
-(println "Hello world!")
-
-(defui HelloWorld
+(defui Counter
+  static om/IQuery
+  (query [this] [:counter])
   Object
   (render [this]
-    (dom/div nil "Hello, World!")))
+          (dom/div nil
+                   (dom/div nil (str "Counter: " (:counter (om/props this))))
+                   (dom/button #js {:onClick
+                                    #(mutate!
+                                       `[(ui/increment {:value 1}) :counter])}
+                               "Increment"))))
 
-(def hello (om/factory HelloWorld))
-
-(js/ReactDOM.render (hello) (gdom/getElement "main-app-area"))
+(om/add-root! reconciler
+  Counter (gdom/getElement "main-app-area"))
